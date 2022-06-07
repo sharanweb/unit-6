c359be5a06de6f36adc343c56b015025a758892c
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-06-07 07:51:36 +0530

    dom appended initial

---
+++ front-back/client/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import {List} from "./Components/productlist.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <List></List>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-back/client/src/Components/productlist.jsx
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const List = () =>{
+    const [data, setData] = useState([]);
+
+    const getData = ()=>{
+        axios.get("https://frontback1.herokuapp.com/products").then(({ data }) => {
+      setData(data.product);
+    })
+    console.log(data);
+
+    }
+    useEffect(()=>{
+      getData();
+      // handleChange();
+      // handleRemove(); 
+    },[])
+
+
+    return (
+        <>
+        {
+            data.map((el)=>(
+                <div className="product__card" key={el._id}>
+                    <div className="imgDiv">
+                        <img src={el.img_url} alt="" className="img" />
+                    </div>
+                    <div className="main_info">
+                        <h3 className="product__brand">{el.brand}</h3>
+                        <h4 className="product__title">{el.price}</h4>
+                    </div>
+                </div>
+            ))
+        }
+        </>
+    )
+
+}
